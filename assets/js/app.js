3db77577ebe61cee1826ae2ef0cdf9be9c00e0b8
gisonline <oskooei@gmail.com>
2023-08-07 13:39:25 +0330

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -845,6 +845,7 @@ const requestWakeLock = async () => {
 //        wakeLockCheckbox.checked = true;
 //        statusDiv.textContent = 'Wake Lock is active';
         console.log('Wake Lock is active');
+        alert('awake')
     } catch (e) {
 //        wakeLockCheckbox.checked = false;
 //        statusDiv.textContent = `${e.name}, ${e.message}`;
